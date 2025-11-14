38c7323f438253bd87bd2f3870f3e6317832f9e4
kithub <punyapat.kit2000@gmail.com>
2025-11-14 13:48:15 +0700

    feat: add repository mock

---
+++ Backend-Golang/v1/internal/repository/inventoryRepository_test.go
@@ -0,0 +1,28 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/PPunyapatt/InventoryService-Vue-Golang/v1/internal/constant"
+	"github.com/PPunyapatt/InventoryService-Vue-Golang/v1/internal/helper"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockInventoryRepo struct {
+	mock.Mock
+}
+
+func (m *MockInventoryRepo) ListInventories(ctx context.Context, p *helper.Pagination) ([]*constant.Inventories, error) {
+	args := m.Called(ctx, p)
+	return args.Get(0).([]*constant.Inventories), args.Error(1)
+}
+
+func (m *MockInventoryRepo) AddInventory(ctx context.Context, code string) error {
+	args := m.Called(ctx, code)
+	return args.Error(0)
+}
+
+func (m *MockInventoryRepo) DeleteInventory(ctx context.Context, code string) error {
+	args := m.Called(ctx, code)
+	return args.Error(0)
+}
