56a95e813c7621a8ce12ade169ee5d93ef0ea774
kithub <punyapat.kit2000@gmail.com>
2025-11-17 17:38:06 +0700

    feat: add nginx for proxy to service

--- Frontend-Vue/src/http.js
+++ Frontend-Vue/src/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: import.meta.env.VITE_API_ENDPOINT || "http://localhost:1234/api/v1",
+  baseURL: "/api",
   headers: {
     "Content-type": "application/json"
   }
